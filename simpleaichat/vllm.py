d7e27660ad4ce4210fc1d151a036634b5e1125f0
psych0v0yager <ian.matejka@gmail.com>
2024-07-20 17:21:26 -0500

    Added async variants

--- simpleaichat/vllm.py
+++ simpleaichat/vllm.py
@@ -233,3 +233,165 @@ class vLLMSession(ChatSession):
         self.add_messages(user_message, assistant_message, save_messages)
 
         return context_dict
+
+    async def gen_async(
+        self,
+        prompt: str,
+        client: Union[Client, AsyncClient],
+        system: str = None,
+        save_messages: bool = None,
+        params: Dict[str, Any] = None,
+        input_schema: Any = None,
+        output_schema: Any = None,
+    ):
+        headers, data, user_message = self.prepare_request(
+            prompt, system, params, False, input_schema, output_schema
+        )
+
+        r = await client.post(
+            str(self.api_url),
+            json=data,
+            headers=headers,
+            timeout=None,
+        )
+        r = r.json()
+
+        try:
+            if not output_schema:
+                content = r["choices"][0]["message"]["content"]
+                assistant_message = ChatMessage(
+                    role=r["choices"][0]["message"]["role"],
+                    content=content,
+                    finish_reason=r["choices"][0]["finish_reason"],
+                    prompt_length=r["usage"]["prompt_tokens"],
+                    completion_length=r["usage"]["completion_tokens"],
+                    total_length=r["usage"]["total_tokens"],
+                )
+                self.add_messages(user_message, assistant_message, save_messages)
+            else:
+                content = r["choices"][0]["message"]["content"]
+                content = orjson.loads(content)
+
+            self.total_prompt_length += r["usage"]["prompt_tokens"]
+            self.total_completion_length += r["usage"]["completion_tokens"]
+            self.total_length += r["usage"]["total_tokens"]
+        except KeyError:
+            raise KeyError(f"No AI generation: {r}")
+
+        return content
+
+    async def stream_async(
+        self,
+        prompt: str,
+        client: Union[Client, AsyncClient],
+        system: str = None,
+        save_messages: bool = None,
+        params: Dict[str, Any] = None,
+        input_schema: Any = None,
+        output_schema: Any = None,
+    ):
+        headers, data, user_message = self.prepare_request(
+            prompt, system, params, True, input_schema, output_schema
+        )
+
+        async with client.stream(
+            "POST",
+            str(self.api_url),
+            json=data,
+            headers=headers,
+            timeout=None,
+        ) as r:
+            content = []
+            async for chunk in r.aiter_lines():
+                if len(chunk) > 0:
+                    chunk = chunk[6:]  # SSE JSON chunks are prepended with "data: "
+                    if chunk != "[DONE]":
+                        chunk_dict = orjson.loads(chunk)
+                        delta = chunk_dict["choices"][0]["delta"].get("content")
+                        if delta:
+                            content.append(delta)
+                            yield {"delta": delta, "response": "".join(content)}
+
+        # streaming does not currently return token counts
+        assistant_message = ChatMessage(
+            role="assistant",
+            content="".join(content),
+        )
+
+        self.add_messages(user_message, assistant_message, save_messages)
+
+        return assistant_message
+
+    async def gen_with_tools_async(
+        self,
+        prompt: str,
+        tools: List[Any],
+        client: Union[Client, AsyncClient],
+        system: str = None,
+        save_messages: bool = None,
+        params: Dict[str, Any] = None,
+    ) -> Dict[str, Any]:
+        
+        # Call 1: Select the correct tool
+        # vLLM supports guided choice, we can now directly pass in tool names
+        tool_names = [tool.__name__ for tool in tools]
+        tool_names.append("no-function")
+
+        # Add the list of tool names to the tool prompt
+        tools_list = "\n".join(tool_names)
+        tool_prompt_format = tool_prompt.format(tools=tools_list)
+
+        # Use guided_choice to let the model select a tool
+        tool_choice = await self.gen_async(
+            prompt,
+            client=client,
+            system=tool_prompt_format,
+            save_messages=False,
+            params={
+                "temperature": 0.0,
+                "guided_choice": tool_names,
+            },
+        )
+
+        # If no tool is selected, do a standard generation instead.
+        if tool_choice == "no-function":
+            return {
+                "response": await self.gen_async(
+                    prompt,
+                    client=client,
+                    system=system,
+                    save_messages=save_messages,
+                    params=params,
+                ),
+                "tool": None,
+            }
+        
+        # Execute the selected tool
+        selected_tool = next(tool for tool in tools if tool.__name__ == tool_choice)
+        context_dict = await selected_tool(prompt)
+        if isinstance(context_dict, str):
+            context_dict = {"context": context_dict}
+
+        context_dict["tool"] = selected_tool.__name__
+
+        # Call 2: Incorporate the tool response into the model for a new generation
+        # This creates a temporary vLLMSession
+        new_system = f"{system or self.system}\n\nYou MUST use information from the context in your response."
+        new_prompt = f"Context: {context_dict['context']}\n\nUser: {prompt}"
+
+        context_dict["response"] = await self.gen_async(
+            new_prompt,
+            client=client,
+            system=new_system,
+            save_messages=False,
+            params=params,
+        )
+
+        # Manually append the nonmodified user message + normal AI response
+        user_message = ChatMessage(role="user", content=prompt)
+        assistant_message = ChatMessage(
+            role="assistant", content=context_dict["response"]
+        )
+        self.add_messages(user_message, assistant_message, save_messages)
+
+        return context_dict
